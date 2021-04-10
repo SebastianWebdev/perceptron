48385332d480953caa7fe28f38711fbfba84864f
Sebastian Gołębiowski <se.golebiowski@gmail.com>
2021-04-10 15:33:41 +0200

    work

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
 
-
 class Perceptron:
-    def __init__(self, activation_value, correction_value=1, dimension=1):
+    def __init__(self, activation_value, correction_value=0.1, dimension=1):
         self.correction = correction_value
         self.activationValue = activation_value
         self.inputs = []
@@ -15,12 +14,24 @@ class Perceptron:
 
     def sumator(self):
         i = 0
+        sum = 0
         for u in self.inputs:
-            self.sum += u * self.weights[i]
+            sum += u * self.weights[i]
             i += 1
+        return sum - self.activationValue
 
-    def activation(self):
-        if self.sum - self.activationValue >= 0:
+    def print_data(self):
+        print("")
+        print("----------------------")
+        print("Wagi:", self.weights)
+        print("Inputs:", self.inputs)
+        print("Suma:", self.sum)
+        print("Wynik:", self.output)
+        print("-----------------------")
+        print("")
+
+    def activation(self, sum):
+        if sum >= 0:
             self.output = 1
         else:
             self.output = 0
@@ -29,33 +40,42 @@ class Perceptron:
         if len(inputs) != self.dimension:
             raise Exception("Liczba wejść musi być równa:" + str(self.dimension))
         self.inputs = inputs
-        self.sumator()
-        self.activation()
+        sum = self.sumator()
+        self.activation(sum)
+
         return self.output
 
     def learn(self, learning_data):
         for U in learning_data:
             local_inputs = U[0]
             correct_output = U[1]
-
             result = self.compute(local_inputs)
-            if result != correct_output:
-                i = 0
-                for w in self.weights:
-                    new_weight = w + self.correction * local_inputs[i] * (correct_output - result)
-                    self.weights[i] = new_weight
+            i = 0
+
+            print(self.weights)
+            for w in self.weights:
+                print(w)
+                new_weight = w + self.correction * local_inputs[i] * (correct_output - result)
+                self.weights[i] = new_weight
+                i += 1
+
 
 
 learning_dat = [[[0, 0], 0], [[0, 1], 0], [[1, 0], 0], [[1, 1], 1]]
 
-and_perceptron = Perceptron(0.2, 0.1, 2)
+and_perceptron = Perceptron(0.7, 0.1, 2)
 
 and_perceptron.learn(learning_dat)
 and_perceptron.learn(learning_dat)
+and_perceptron.learn(learning_dat)
+and_perceptron.learn(learning_dat)
+and_perceptron.learn(learning_dat)
+and_perceptron.learn(learning_dat)
+
+
 
 
 print(and_perceptron.compute([0, 0]), "output: 0")
 print(and_perceptron.compute([0, 1]), "output: 0")
 print(and_perceptron.compute([1, 0]), "output: 0")
 print(and_perceptron.compute([1, 1]), "output: 1")
-
